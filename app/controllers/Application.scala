ea7ca7622b307ac12d96c34c2bbfdb4c18603f5d
Alan Chusuei <alan.chusuei@gmail.com>
2014-09-06 00:21:28 -0400

    add js files, setup require.js

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -9,4 +9,12 @@ object Application extends Controller {
     Ok(views.html.index("Your new application is ready."))
   }
 
+  def game = Action {
+    Ok(views.html.game())
+  }
+
+  def bootstrap = Action {
+    Ok(views.html.bootstrap())
+  }
+
 }
